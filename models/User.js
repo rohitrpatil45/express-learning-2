74bd6f34d3a580603510342e45edee7d9c161363
Rohit Patil <rohitrajupatil12@gmail.com>
2025-09-14 20:41:06 +0530

    afsd

--- models/User.js
+++ models/User.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
-// schema = rules/blueprint for users
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true }, 
-  password: { type: String, required: true }
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-// model = actual object to work with in code
-const User = mongoose.model("User", userSchema);
-
-export default User;
+export default mongoose.model("User", userSchema);
--- sever.js
+++ sever.js
@@ -2,96 +2,99 @@ import express from "express"
 import ConnectDB from "./db.js"
 import User from "./models/User.js";   // <-- here
 import Product from "./models/Product.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 
 const app = express()
 app.use(express.json()); // handles JSON requests
 app.use(express.urlencoded({ extended: true })); // handles form-data (urlencoded)
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"]; 
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  jwt.verify(token, "mysecret", (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    req.user = decoded; // attach user data to req
+    next();
+  });
+}
 
 const PORT = 3000
 
 app.get('/', (req, res) => {
-  res.send("hello world")
+  res.send("hello  world")
 })
+
 
 ConnectDB()
 
 
 
-app.post('/add-product', async (req, res) => {
+app.post('/register', async (req, res) => {
   try {
-    const { productname, productprice } = req.body;
-    const productexist = await Product.findOne({ productname })
+    const { username, email, password } = req.body
+    const Passwordhashing = await bcrypt.hash(password, 10)
 
+    const Newuser = new User({
+      username,
+      email,
+      password: Passwordhashing
+    })
 
-    if (productexist) {
-      return res.status(400).json({ message: "this product is already exist" })
-    }
-
-    const newPorduct = new Product({ productname, productprice });
-    await newPorduct.save()
-    res.status(201).json({ message: "prodsduct is added" })
+    await Newuser.save()
+    console.log("here")
+    res.status(201).json({ message: "register scussfully" })
   } catch (error) {
-    res.status(500).json({ errorr: error.message })
+    res.status(500).json({ error: error.message });
 
   }
+})
 
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // email checking 
+    const userchecking = await User.findOne({ email })
+    if (!userchecking) {
+      return res.json({ message: "a re tuzi email wrong hy" })
+    }
+    // password checking 
+    const passwordchecking = await bcrypt.compare(password, userchecking.password)
+    if (!passwordchecking) {
+      return res.json({message: " a re lavdu , password wrong hy "})
+
+    }
+
+    const token = jwt.sign(
+      {id:userchecking._id},
+      "mysecret",
+      {expiresIn: "1h"}
+    )
+
+    res.json({message:"kadak re tu login  kels", token})
+
+
+  } catch (error) {
+    res.status(500).json({message: error.message})
+
+  }
 })
 
 
-// app.put('/update-product/:id', async (req, res) => {
-//   try {
-//     const productId = req.params.id;
-//     const { price } = req.body;
-
-//     const updateproduct = await Product.findByIdAndUpdate(productId, { price }, { new: true, runValidators: true })
-//     if (!updateproduct) {
-//       res.status(404).json({ message: "proudcut id ieas wrong " })
-//     }
-//   } catch (error) {
-//     console.log(error)
-//   }
-
-
-// })
-
-
-app.put('/update-product/:id', async (req, res) => {
-  try {
-    const productId = req.params.id;
-    const { price } = req.body;
-
-    // Update product by ID
-    const updatedProduct = await Product.findByIdAndUpdate(
-      productId,
-      { productprice: price },   // make sure field name matches schema
-      { new: true, runValidators: true }
-    );
-
-    if (!updatedProduct) {
-      return res.status(404).json({ message: "Product ID is wrong" });
-    }
-
-    res.json({
-      message: "Product updated successfully",
-      product: updatedProduct
-    });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
+app.get("/profile", authMiddleware, async (req, res) => {
+  const user = await User.findById(req.user.id).select("-password"); // hide password
+  res.json({ message: "Profile fetched", user });
 });
 
-
-// app.get('/product' , async (req, res) => {
-//  try {
-//   const allproduct =  await Product.find({},"productname")
-//   console.log(allproduct)
-//   res.send("check the console ")
-//  } catch (error) {
-//   res.send("this is done ")
-//  }
-// })
 
 
 app.listen(PORT, () => {
