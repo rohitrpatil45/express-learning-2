182ed210049d98991144b9a5d6c1441237f91faa
Rohit Patil <rohitrajupatil12@gmail.com>
2025-09-15 12:03:12 +0530

    auth is working on this time

--- sever.js
+++ sever.js
@@ -1,16 +1,15 @@
-import express from "express"
-import ConnectDB from "./db.js"
-import User from "./models/User.js";   // <-- here
+import express from "express";
+import ConnectDB from "./db.js";
+import User from "./models/User.js"; // <-- here
 import Product from "./models/Product.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-
-const app = express()
+const app = express();
 app.use(express.json()); // handles JSON requests
 app.use(express.urlencoded({ extended: true })); // handles form-data (urlencoded)
 function authMiddleware(req, res, next) {
-  const authHeader = req.headers["authorization"]; 
+  const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
 
   if (!token) {
@@ -27,78 +26,67 @@ function authMiddleware(req, res, next) {
   });
 }
 
-const PORT = 3000
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send("hello  world")
-})
+app.get("/", (req, res) => {
+  res.send("hello  world");
+});
 
+ConnectDB();
 
-ConnectDB()
-
-
-
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   try {
-    const { username, email, password } = req.body
-    const Passwordhashing = await bcrypt.hash(password, 10)
+    const { username, email, password } = req.body;
+    const Passwordhashing = await bcrypt.hash(password, 10);
 
     const Newuser = new User({
       username,
       email,
-      password: Passwordhashing
-    })
+      password: Passwordhashing,
+    });
 
-    await Newuser.save()
-    console.log("here")
-    res.status(201).json({ message: "register scussfully" })
+    await Newuser.save();
+    // console.log("here")
+    res.status(201).json({ message: "register scussfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
-
   }
-})
+});
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // email checking 
-    const userchecking = await User.findOne({ email })
+    // email checking
+    const userchecking = await User.findOne({ email });
     if (!userchecking) {
-      return res.json({ message: "a re tuzi email wrong hy" })
+      return res.json({ message: "a re tuzi email wrong hy" });
     }
-    // password checking 
-    const passwordchecking = await bcrypt.compare(password, userchecking.password)
+    // password checking
+    const passwordchecking = await bcrypt.compare(
+      password,
+      userchecking.password
+    );
     if (!passwordchecking) {
-      return res.json({message: " a re lavdu , password wrong hy "})
-
+      return res.json({ message: " a re lavdu , password wrong hy " });
     }
 
-    const token = jwt.sign(
-      {id:userchecking._id},
-      "mysecret",
-      {expiresIn: "1h"}
-    )
+    const token = jwt.sign({ id: userchecking._id }, "mysecret", {
+      expiresIn: "1h",
+    });
 
-    res.json({message:"kadak re tu login  kels", token})
-
-
+    res.json({ message: "kadak re tu login  kels", token });
   } catch (error) {
-    res.status(500).json({message: error.message})
-
+    res.status(500).json({ message: error.message });
   }
-})
-
+});
 
 app.get("/profile", authMiddleware, async (req, res) => {
   const user = await User.findById(req.user.id).select("-password"); // hide password
   res.json({ message: "Profile fetched", user });
+  console.log(user.username)
 });
 
-
-
 app.listen(PORT, () => {
-
-  console.log("server is running")
-
-})
+  console.log("server is running");
+});
